cd893a035fc7b45cd6292a0008d49fe411056adb
Arturo Arroyo <turo310@gmail.com>
2021-04-20 22:44:11 -0700

    Add auth context provider

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { useContext } from 'react';
 import Link from 'next/link';
 import Search from './Search';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/Header.module.css';
 
 export default function Header() {
+    const {user, logout} = useContext(AuthContext);
+
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
@@ -21,11 +25,28 @@ export default function Header() {
                             <a>Events</a>
                         </Link>
                     </li>
-                    <li>
+                    {user ? 
+                    // If logged in
+                    (<>
+                        <li>
                         <Link href='/events/add'>
                             <a>Add Event</a>
                         </Link>
                     </li>
+                        <li>
+                        <Link href='/account/dashboard'>
+                            <a>Dashboard</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <button className='btn-secondary btn-icon' onClick={() => logout()}>
+                            <FaSignOutAlt /> Logout
+                        </button>
+                    </li>
+                    </> 
+                    ) : (
+                        // If logged out
+                    <>
                     <li>
                         <Link href='/account/login'>
                             <a className='btn-secondary btn-icon'>
@@ -33,6 +54,8 @@ export default function Header() {
                             </a>
                         </Link>
                     </li>
+                    </>
+                    )}
                 </ul>
             </nav>
         </header>
--- pages/account/login.js
+++ pages/account/login.js
@@ -4,14 +4,18 @@ import 'react-toastify/dist/ReactToastify.css'
 import {useState, useEffect, useContext} from 'react';
 import Link from 'next/link';
 import Layout from '@/components/Layout';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/AuthForm.module.css';
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const {login, error} = useContext(AuthContext);
+
     const handleSubmit = (e) => {
         e.preventDefailt();
+        login({email, password});
     }
 
     return (
