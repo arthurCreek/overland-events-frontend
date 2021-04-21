121d97d9336032b79ab5d080f461c90d76ddeba4
Arturo Arroyo <turo310@gmail.com>
2021-04-20 23:05:48 -0700

    Login and get JWT

--- pages/account/login.js
+++ pages/account/login.js
@@ -13,35 +13,47 @@ export default function LoginPage() {
 
     const {login, error} = useContext(AuthContext);
 
+    useEffect(() => error && toast.error(error));
+
     const handleSubmit = (e) => {
-        e.preventDefailt();
+        e.preventDefault();
         login({email, password});
     }
 
     return (
-        <Layout title='User Login'>
-            <div className={styles.auth}>
-                <h1>
-                    <FaUser /> Login
-                </h1>
-                <ToastContainer />
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="email">Email Address</label>
-                        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" value={email} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
+    <Layout title='User Login'>
+      <div className={styles.auth}>
+        <h1>
+          <FaUser /> Log In
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              type='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-                    <input type="submit" value="Login" className='btn'></input>
+          <input type='submit' value='Login' className='btn' />
+        </form>
 
-                    <p>
-                        Don't have an account? <Link href='/account/register'>Register</Link>
-                    </p>
-                </form>
-            </div>
-        </Layout>
+        <p>
+          Don't have an account? <Link href='/account/register'>Register</Link>
+        </p>
+      </div>
+    </Layout>
     )
 }
